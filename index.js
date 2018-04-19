931677720df4c8e6ba4dac37a108613082e1b01e
Nitin <s.nani100@gmail.com>
2018-04-19 12:28:03 +0530

    asdf

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ exports.handler = (event, context, callback) => {
     currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
-        body: 'The time in Los Angeles is bla: ' + currentTime.toString(),
+        body: 'The time in Los Angeles is bla:' + currentTime.toString(),
     });
 };
